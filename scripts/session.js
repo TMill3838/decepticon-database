634f3b37edf86d7ef3466242d28da34d506b98c8
TMill3838 <rabbitgolden101@gmail.com>
2026-03-09 18:07:27 -0500

    session.js

--- scripts/session.js
+++ scripts/session.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const authLinks = document.getElementById("auth-links");
 
     if (isLoggedIn === "true") {
-        // User is logged in: Show Logout button
+       
         if (authLinks) {
             authLinks.innerHTML = `
                 <li class="nav-item"><span class="nav-link text-info">Rise Up, ${localStorage.getItem("currentDecepticonUser")}!</span></li>
@@ -12,8 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
             `;
         }
     } else {
-        // User is NOT logged in: Ensure they only see Login
+        
         if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
         }
     }
+
 });
